dca0869ec22e740060871f08882e4af48b35af5f
Vincenzo Esposito <expovin@gmail.com>
2025-05-03 21:14:39 +0400

    Sostitusione del REST SErver strategyUtil con una libreria condivisa e aggiunta del logger in tutti i docker con settaggio del livello per singolo docker

--- alertingService/alertingService.js
+++ alertingService/alertingService.js
@@ -1,9 +1,11 @@
 // AlertingService.js
 const nodemailer = require('nodemailer');
 const axios = require('axios');
+const createLogger = require('../shared/logger');
 
 const MODULE_NAME = 'AlertingService';
 const MODULE_VERSION = '1.0';
+const logger = createLogger(MODULE_NAME, process.env.LOG_LEVEL || 'info');
 
 class AlertingService {
   constructor() {
@@ -15,15 +17,20 @@ class AlertingService {
     const keys = ['SMTP_HOST', 'SMTP_PORT', 'SMTP_USER', 'SMTP_PASSWORD', 'SMTP_FROM'];
     const settings = {};
     const dbManagerUrl = process.env.DBMANAGER_URL || 'http://dbmanager:3002';
+    logger.trace(`[loadSetting] Recupero settings da ${dbManagerUrl}`);
 
     for (const key of keys) {
-      try {
-        const res = await axios.get(`${dbManagerUrl}/getSetting/${key}`);
-        settings[key] = res.data.value;
-      } catch (err) {
-        console.error(`[${MODULE_NAME}][loadSettings] Errore nella chiave ${key}: ${err.message}`);
-        throw err;
-      }
+        logger.trace(`[loadSetting] loop chiave ${key}`);
+        try {
+          logger.trace(`[loadSetting] Chiamo url ${dbManagerUrl}/getSetting/${key}`);
+          const res = await axios.get(`${dbManagerUrl}/getSetting/${key}`);
+          console.log(res.data.value);
+          settings[key] = res.data.value;
+          logger.trace(`[loadSetting] Setting variavile ${key} : ${settings[key]}`);
+        } catch (err) {
+          logger.error(`[loadSettings] Errore nella chiave ${key}: ${err.message}`);
+          throw err;
+        }
     }
 
     this.smtpFrom = settings.SMTP_FROM;
@@ -38,24 +45,25 @@ class AlertingService {
       }
     });
 
-    console.log(`[${MODULE_NAME}] Configurazione SMTP caricata.`);
+    logger.info(`[loadSetting] Configurazione SMTP caricata.`);
   }
 
   // Invio email
   async sendEmail({ to, subject, body }) {
-    try {
-      const info = await this.transporter.sendMail({
-        from: this.smtpFrom,
-        to:to,
-        subject:subject,
-        text: body
-      });
-      console.log(`[${MODULE_NAME}][sendEmail] Email inviata a ${to}: ${info.messageId}`);
-      return info;
-    } catch (err) {
-      console.error(`[${MODULE_NAME}][sendEmail] Errore invio email: ${err.message}`);
-      throw err;
-    }
+      logger.log(`[sendEmail] richiamata con parametri ${to} ${subject} ${body}`);
+      try {
+        const info = await this.transporter.sendMail({
+          from: this.smtpFrom,
+          to:to,
+          subject:subject,
+          text: body
+        });
+        logger.log(`[${MODULE_NAME}][sendEmail] Email inviata a ${to}: ${info.messageId}`);
+        return info;
+      } catch (err) {
+        logger.error(`[${MODULE_NAME}][sendEmail] Errore invio email: ${err.message}`);
+        throw err;
+      }
   }
 
   // Informazioni sul modulo
@@ -63,6 +71,7 @@ class AlertingService {
     return {
       module: MODULE_NAME,
       version: MODULE_VERSION,
+      logLevel: process.env.LOG_LEVEL,
       status: 'OK'
     };
   }
