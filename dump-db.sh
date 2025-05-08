e095cc8ca9f82e13d96cc7e1a059bbce7de1139e
Vincenzo Esposito <expovin@gmail.com>
2025-05-08 21:13:49 +0400

    Test pipe CI/CD

---
+++ dump-db.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Configurazioni
+DB_NAME="Trading"
+DB_USER="root"
+DB_PASS="example"
+DB_HOST="127.0.0.1"
+DB_PORT="3316"
+TIMESTAMP=$(date +"%Y%m%d-%H%M%S")
+OUTPUT_DIR="./db"
+OUTPUT_FILE="$OUTPUT_DIR/tradingDb-${DB_NAME}-${TIMESTAMP}.sql"
+
+# Assicurati che la cartella di output esista
+# mkdir -p "$OUTPUT_DIR"
+
+# Dump del database
+echo "📦 Dump del database '$DB_NAME' su porta $DB_PORT → $OUTPUT_FILE"
+mysqldump --protocol=TCP -h "$DB_HOST" -P "$DB_PORT" -u "$DB_USER" -p"$DB_PASS" --skip-lock-tables --extended-insert "$DB_NAME" > "$OUTPUT_FILE"
+
+# Verifica esito
+if [ $? -eq 0 ]; then
+  echo "✅ Dump completato con successo."
+else
+  echo "❌ Errore durante il dump."
+  exit 1
+fi
